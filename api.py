6ff1f4b1963828957c8f229d7c6cfc03c645d0b2
justfortestingnothibghere <armanhacker900@gmail.com>
2026-04-01 18:47:48 +0530

    Update api.py

--- api.py
+++ api.py
@@ -98,4 +98,4 @@ threading.Thread(target=self_ping, daemon=True).start()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 3500)))
